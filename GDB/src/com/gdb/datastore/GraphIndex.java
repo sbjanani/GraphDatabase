61cb5044fd4a001574662da85fee0fc2329508c1
janani <janani@131.96.48.253>
2014-06-05 14:07:26 -0400

    Added file GraphStart.java. Created code skeletons in GraphIndex.java

--- GDB/src/com/gdb/datastore/GraphIndex.java
+++ GDB/src/com/gdb/datastore/GraphIndex.java
@@ -1,5 +1,38 @@
+/**
+ * This file populates the "graph.idx" file
+ * @author janani
+ *
+ */
 package com.gdb.datastore;
+
+import java.util.ArrayList;
 
 public class GraphIndex {
 
+	
+	/**
+	 * This method reads the input file and creates an ArrayList with the edge information
+	 * @param path - location where the input files are stored
+	 * @return
+	 */
+	public ArrayList<int []> readGraph(String path){
+		
+		ArrayList<int[]> edgeArray = new ArrayList<int[]>();
+		
+		// code goes here
+		
+		return edgeArray;
+		
+	}
+	
+	/**
+	 * This method writes the node index information into the file graph.idx 
+	 * @param path - location where the output file "graph.idx" is stored
+	 * @param edgeArray - arraylist containing the edge information
+	 */
+	public void writeGraph(String path, ArrayList<int[]> edgeArray){
+		
+		// code goes here
+		
+	}
 }
---
+++ GDB/src/com/gdb/datastore/GraphStart.java
@@ -0,0 +1,16 @@
+package com.gdb.datastore;
+
+/**
+ * @author janani
+ *
+ */
+public class GraphStart {
+
+	/**
+	 * This is the starting point of the application
+	 * @param args
+	 */
+	public static void main(String args[]){
+		
+	}
+}
