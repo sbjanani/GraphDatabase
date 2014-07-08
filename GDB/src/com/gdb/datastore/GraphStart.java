5442f96c2c369f70d9556a5ed94ff51e33fb69d4
Naveen <Naveen@Macintosh.local>
2014-07-08 10:57:22 -0400

    commit

--- GDB/src/com/gdb/datastore/GraphStart.java
+++ GDB/src/com/gdb/datastore/GraphStart.java
@@ -15,15 +15,8 @@ public class GraphStart {
          * @throws IOException 
          */
         public static void main(String args[]) throws IOException{
-                GraphIndex gi = new GraphIndex("nodes2.dat");
-                ArrayList<AdjacencyRecord> a = gi.readGraph("rel.dat");
-                //System.out.println(a);
-                //for(int i = 0; i < a.size(); i++){
-                /*for(int i = 0; i < 5; i++){
-                        System.out.print("i= "+i);
-                        System.out.println(a.get(i));
-                }*/
-                GraphIndex g = new GraphIndex("nodes2.dat");
-                g.writeGraph(args[0],a);
+                GraphIndex gi = new GraphIndex("nodesInput.dat");
+                ArrayList<AdjacencyRecord> a = gi.readGraph("edgesInput.dat");
+                gi.writeGraph(args[0],a);
         }
 }
